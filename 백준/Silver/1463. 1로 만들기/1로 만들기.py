688794ede52c561238a3bda1f62e62b477373f1c
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2025-11-18 10:13:09 +0900

    [Silver III] Title: 1로 만들기, Time: 568 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [0] * (n+1)
+
+for i in range(2, n+1):
+    candidates = []
+    if i % 3 == 0:
+        candidates.append(dp[i//3] + 1)
+    if i % 2 == 0:
+        candidates.append(dp[i//2] + 1)
+    candidates.append(dp[i-1] + 1)
+
+    dp[i] = min(candidates)
+
+print(dp[n])
