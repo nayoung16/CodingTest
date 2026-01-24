e0b0d829ef3c7c3afdce18ea533aaa354d9acb86
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-01-24 11:26:24 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,20 @@
+n,m = map(int, input().split())
+result = []
+visited = [False for _ in range(n+1)]
+
+def dfs(depth):
+    if depth == m:
+        print(*result)
+        return
+
+    for i in range(1,n+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        result.append(i)
+
+        dfs(depth+1)
+        result.pop()
+        visited[i] = False
+
+dfs(0)
