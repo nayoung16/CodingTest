aaaa80dac209e84921dc3d2ef83d6edd2bfb8c4a
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-01-24 11:39:12 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,6 +1,21 @@
-from itertools import combinations
 n,m = map(int, input().split())
-numbers = [i for i in range(1,n+1)]
-results = combinations(numbers,m)
-for result in results:
-    print(*result)
+result = []
+visited = [False for _ in range(n+1)]
+
+def dfs(depth):
+    if depth == m:
+        print(*result)
+        return
+
+    for i in range(1,n+1):
+        if visited[i]:
+            continue
+        if len(result) > 0 and i > result[-1] or result == []:
+            visited[i] = True
+            result.append(i)
+
+            dfs(depth+1)
+            result.pop()
+            visited[i] = False
+
+dfs(0)
