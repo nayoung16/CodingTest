56abcdfe10a48216e0fa6df9c5ad674f9e9baf82
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-01-26 11:38:43 +0900

    [Silver V] Title: 줄세우기, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -6,15 +6,11 @@ for _ in range(p):
     answer = 0
     for i in range(0,20):
         cur_student = students[i]
-        new_student = []
         for j in range(0,i):
             if students[j] > cur_student:
                 # 물러난 걸음 수 : i-j
                 answer += (i-j)
-                new_student = students[:j]
-                new_student.append(cur_student)
-                new_student += students[j:i]
-                new_student += students[i+1:]
-                students = new_student
+                students.pop(i)
+                students.insert(j, cur_student)
                 break
     print(f"{t_num} {answer}")
