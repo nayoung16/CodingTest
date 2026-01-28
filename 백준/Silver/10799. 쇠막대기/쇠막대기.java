3bd886873c57149ace01b0b91328fc54bb7dcc92
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-01-28 14:18:35 +0900

    [Silver II] Title: 쇠막대기, Time: 136 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -6,32 +6,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        Stack<Character> stack = new Stack<>();
         int bar_num = 0; // 현재 막대기 개수
         int answer = 0; // 잘려진 막대기 총 개수
         for (int i=0; i < str.length(); i++) {
             char c = str.charAt(i);
             if (c == '(') {
-                if (str.charAt(i+1) != ')') {
-                    // 레이저 아님, 막대기
-                    bar_num++;
-                    continue;
-                }
+                bar_num++;
             }
-            else if (c == ')') {
-                char s = stack.peek();
-                if (s == '(') {
-                    // 레이저
+            else {
+                bar_num--;
+                if (str.charAt(i - 1) == '(') {
+                    // 레이저 "()" -> 현재 열려있는 막대 bar_num개를 자름
                     answer += bar_num;
-                    stack.pop();
-                }
-                else {
+                } else {
                     // 막대기 끝
                     answer++;
-                    bar_num--;
                 }
             }
-            stack.push(c);
         }
         System.out.println(answer);
     }
