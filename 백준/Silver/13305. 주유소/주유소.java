19e37a6accf2d5e14b1e9013656b26cc2080b40c
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-02-14 21:20:02 +0900

    [Silver III] Title: 주유소, Time: 368 ms, Memory: 36424 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -3,28 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-import static java.lang.Math.min;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] roads = new int[n-1];
+        long[] roads = new long[n-1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n-1; i++) {
-            roads[i] = Integer.parseInt(st.nextToken());
+            roads[i] = Long.parseLong(st.nextToken());
         }
 
-        int[] gas = new int[n];
+        long[] gas = new long[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            gas[i] = Integer.parseInt(st.nextToken());
+            gas[i] = Long.parseLong(st.nextToken());
         }
 
-        int minPrice = gas[0];
-        int result = 0;
+        long minPrice = gas[0];
+        long result = 0L;
         for (int i = 0; i < n-1; i++) {
-            minPrice = min(minPrice, gas[i]);
+            minPrice = Math.min(minPrice, gas[i]);
             result += minPrice * roads[i];
         }
         System.out.println(result);
