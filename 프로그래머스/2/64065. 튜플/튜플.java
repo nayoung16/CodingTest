5c29c0704b445a14bb48e438f5d4943da38b13c6
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-02-15 10:48:46 +0900

    [level 2] Title: 튜플, Time: 209.73 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,51 +1,25 @@
 import java.util.*;
 
 class Solution {
-    static class TupleSet implements Comparable<TupleSet> {
-        Set<Integer> set;
-        
-        TupleSet(Set<Integer> set) {
-            this.set = set;
-        }
-        
-        @Override
-        public int compareTo(TupleSet o) {
-            return this.set.size() - o.set.size();
-        }
-    }
+
     public int[] solution(String s) {
-        s = s.substring(2, s.length() - 2);
-        String[] parts = s.split("\\},\\{");
+        String newS = s.substring(2,s.length()-2);
+        String[] strings = newS.split("\\},\\{");
         
-        List<TupleSet> list = new ArrayList<>();
-        for (String part : parts) {
-            String[] nums = part.split(",");
-            Set<Integer> set = new HashSet<>();
-            for (String num : nums) {
-                set.add(Integer.parseInt(num));
-            }
-            list.add(new TupleSet(set));
-        }
+        Arrays.sort(strings, (a,b) -> a.split(",").length - b.split(",").length);
         
-        Collections.sort(list);
-        
-        List<Integer> result = new ArrayList<>();
-        Set<Integer> used = new HashSet<>();
-        
-        for (TupleSet ts : list) {
-            for (int x : ts.set) {
-                if (!used.contains(x)) {
-                    used.add(x);
-                    result.add(x);
+        int[] answer = new int[strings.length];
+        Set<Integer> set = new HashSet<>();
+        int idx = 0;
+        for (String string : strings) {
+            for (String num : string.split(",")) {
+                int x = Integer.parseInt(num);
+                if (!set.contains(x)) {
+                    set.add(x);
+                    answer[idx++] = x;
                 }
             }
         }
-        
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
-        
         return answer;
     }
 }
