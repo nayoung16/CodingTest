5eb51fc0162ca459c70a4ca1fbceb6f938ea0c02
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-04-21 13:01:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.26 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 78.66 ms
+메모리: 96.6 MB, 시간: 78.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 02일 11:09:34
+2026년 04월 21일 13:01:45
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,13 +4,13 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> map = new HashMap<>();
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p,0)+1);
+        for (String person : participant) {
+            map.put(person, map.getOrDefault(person,0)+1);
         }
         for (String c : completion) {
             int n = map.get(c) - 1;
-            if (n==0) map.remove(c);
-            else map.put(c,n);
+            if (n > 0) map.put(c,n);
+            else map.remove(c); 
         }
         return map.keySet().iterator().next();
     }
