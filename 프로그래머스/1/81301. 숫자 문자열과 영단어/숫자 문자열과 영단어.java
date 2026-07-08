fe5ad036f9e858b19dbd09f36cff11ab6fe5af8f
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-07-08 09:03:34 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.29 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,38 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        Map<String, Integer> map = new HashMap<>();
-        map.put("zero", 0);
-        map.put("one", 1);
-        map.put("two", 2);
-        map.put("three", 3);
-        map.put("four", 4);
-        map.put("five", 5);
-        map.put("six", 6);
-        map.put("seven", 7);
-        map.put("eight", 8);
-        map.put("nine", 9);
-        
-        int l = s.length();
-        int start = 0;
-        int end = start + 1;
-        while (start < end && end < l+1 && start < l) {
-            char curChar = s.charAt(start);
-            if (Character.isDigit(curChar)) {
-                answer = answer * 10 + (curChar - '0');
-                start = end;
-                end = start + 1;
-                continue;
-            }
-            String substr = s.substring(start, end);
-            if (map.keySet().contains(substr)) {
-                answer = answer * 10 + map.get(substr);
-                start = end;
-                end = start + 1;
-                continue;
-            }
-            end++;
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight" , "nine"};
+        for (int i = 0; i < 10; i++) {
+            s = s.replaceAll(arr[i], String.valueOf(i));
         }
+        
+        if (s.charAt(0) == '0') {
+            s = s.substring(1);
+        }
+        answer = Integer.parseInt(s);
+        
         return answer;
     }
 }
