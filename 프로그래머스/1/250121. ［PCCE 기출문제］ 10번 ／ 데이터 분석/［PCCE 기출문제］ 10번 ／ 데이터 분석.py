0f3f0a0a97d57575902d6cdde7ca7453cdb2b9b4
NayoungKim <102905115+nayoung16@users.noreply.github.com>
2026-09-06 14:39:38 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.07 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,66 +1,12 @@
 def solution(data, ext, val_ext, sort_by):
-    answer = []
+    index = {
+        'code': 0,
+        'date': 1,
+        'maximum': 2,
+        'remain': 3
+    }
     
-    def get_year(data):
-        return data // 10000
-    
-    def get_month(data):
-        return (data % 10000) // 100
-    
-    def get_date(data):
-        return (data % 100)
-    
-    def check(data, index):
-        if data[index] < val_ext:
-            return True
-        return False
-    
-    def check_date(data, val_year, val_month, val_date):
-        cur_year = get_year(data)
-        cur_month = get_month(data)
-        cur_date = get_date(data)
-        if cur_year <= val_year:
-            if cur_month < val_month:
-                return True
-            elif cur_month == val_month:
-                if cur_date < val_date:
-                    return True
-        return False
-    
-    if ext == 'code':
-        for d in data:
-            if check(d, 0):
-                answer.append(d)
-        
-    elif ext == 'date':
-        val_year = get_year(val_ext)
-        val_month = get_month(val_ext)
-        val_date = get_date(val_ext)
-        
-        for d in data:
-            if check_date(d[1], val_year, val_month, val_date):
-                answer.append(d)
-        
-    elif ext == 'maximum':
-        for d in data:
-            if check(d, 2):
-                answer.append(d)
-        
-    elif ext == 'remain':
-        for d in data:
-            if check(d, 3):
-                answer.append(d)
-            
-    key_index = 0
-    if sort_by == 'code':
-        key_index = 0
-    elif sort_by == 'date':
-        key_index = 1
-    elif sort_by == 'maximum':
-        key_index = 2
-    elif sort_by == 'remain':
-        key_index = 3
-        
-    answer.sort(key = lambda x: x[key_index])
+    answer = [d for d in data if d[index[ext]] < val_ext]
+    answer.sort(key=lambda x: x[index[sort_by]])
         
     return answer
